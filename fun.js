fdc428a4eef02f5cd82d4167e3a3fce6c6e5df4f
Gabriel Seals <sealsgj@gmail.com>
2019-09-22 10:28:42 -0500

    baseline MVP. still needs colors depending on temp and enter key functionality

--- fun.js
+++ fun.js
@@ -7,28 +7,28 @@ const printToDom = (divId, toPrint) => {
 const button = document.getElementById("converter");
 const resetButton = document.getElementById("reset");
 
-const toCelsius =  () => {
- 		let temp = document.getElementById('temperature').value;
- 	    return (temp - 32) * 5/9;  
-} 
-//      color designations here
-
 
 const toFahrenheit =  () => {
- 		let temp = document.getElementById('temperature').value;
- 		return (temp * 9/5) + 32;
+	let temp = document.getElementById('temperature').value;
+	let math = (temp * 9/5) + 32;
+	let words = `${math}° Fahrenheit`
+	printToDom ("conversion-space", words)
 }
-//		color designations here
-// }
 
+const toCelsius =  () => {
+ 		let temp = document.getElementById('temperature').value;
+		let math = (temp - 32) * 5/9;
+		let words = `${math}° Celsius`
+		printToDom ("conversion-space", words)
+} 
 
 // // This function should determine which conversion should
 // // happen based on which radio button is selected.
 const determineConverter = (e) => {
 	if (faRadio.checked === true) {
-		return toFahrenheit();
-	} if (celRadio.checked === true) {
-		return toCelsius();
+		toFahrenheit();
+	} else if (celRadio.checked === true) {
+		toCelsius();
 	}
 	//   if cRadio {
 //	 toCelsius
