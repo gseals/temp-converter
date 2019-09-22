c55e334ca7f491cc6110f50ebe205c183444d375
Gabriel Seals <sealsgj@gmail.com>
2019-09-22 10:12:28 -0500

    the clear button works and found radio buttons.checked

--- fun.js
+++ fun.js
@@ -7,44 +7,45 @@ const printToDom = (divId, toPrint) => {
 const button = document.getElementById("converter");
 const resetButton = document.getElementById("reset");
 
-// const toCelsius =  () => {
-// 		let temp = document.getElementById('temperature').value;
-// 	    return (temp - 32) * 5/9;   
+const toCelsius =  () => {
+ 		let temp = document.getElementById('temperature').value;
+ 	    return (temp - 32) * 5/9;  
+} 
+//      color designations here
+
+
+const toFahrenheit =  () => {
+ 		let temp = document.getElementById('temperature').value;
+ 		return (temp * 9/5) + 32;
+}
+//		color designations here
 // }
-
-// const toFahrenheit =  () => {
-// 		let temp = document.getElementById('temperature').value;
-// 		return (temp * 9/5) + 32;
-// }
-
-// button function to initiate either function
-
-// reset button should should erase all entered data 
-
-// // Get a reference to the button element in the DOM
 
 
 // // This function should determine which conversion should
 // // happen based on which radio button is selected.
 const determineConverter = (e) => {
-  let temp = document.getElementById("temperature").value;
-//   if fRadio {
-//   } if cRadio {
+	if (faRadio.checked === true) {
+		return toFahrenheit();
+	} if (celRadio.checked === true) {
+		return toCelsius();
+	}
+	//   if cRadio {
+//	 toCelsius
+//   } if fRadio {
+//	 toFahrenheit
 //   };
-  console.log(temp);
-  printToDom("conversion-space", temp)
+//   printToDom("conversion-space", temp)
 
 }
 
 const clearFields = (e) => {
-	document.getElementById('temperature').innerHTML = '';
-	document.getElementById("conversion-space").innterHTML = '';
+	let burn = document.getElementById('temperature').value = '';
+	printToDom ('conversion-space', '')
 }
-
 
 button.addEventListener("click", determineConverter);
 resetButton.addEventListener("click", clearFields);
-
 
 // enter key has to activate functions
 // colors based on instructions
