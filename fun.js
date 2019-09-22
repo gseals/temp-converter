ef44a4bfdb413872a5118240bec55ec7879cc594
Gabriel Seals <sealsgj@gmail.com>
2019-09-22 09:28:11 -0500

    saving where I am

--- fun.js
+++ fun.js
@@ -1,26 +1,50 @@
-
 console.log('what\'s up danger?')
 
-//we should print the button, as well, so that there isn't a reset button just sitting on the page
+const printToDom = (divId, toPrint) => {
+	document.getElementById(divId).innerHTML = toPrint;
+}
 
-//<button type="submit" id="reset"></button>
+const button = document.getElementById("converter");
+const resetButton = document.getElementById("reset");
 
 // const toCelsius =  () => {
-
+// 		let temp = document.getElementById('temperature').value;
+// 	    return (temp - 32) * 5/9;   
 // }
 
 // const toFahrenheit =  () => {
-
+// 		let temp = document.getElementById('temperature').value;
+// 		return (temp * 9/5) + 32;
 // }
 
+// button function to initiate either function
+
+// reset button should should erase all entered data 
+
 // // Get a reference to the button element in the DOM
-// const button = document.getElementById("converter");
+
 
 // // This function should determine which conversion should
 // // happen based on which radio button is selected.
-// const determineConverter = (e) => {
-//   console.log("event", e);
-// }
+const determineConverter = (e) => {
+  let temp = document.getElementById("temperature").value;
+//   if fRadio {
+//   } if cRadio {
+//   };
+  console.log(temp);
+  printToDom("conversion-space", temp)
 
-// // Assign a function to be executed when the button is clicked
-// button.addEventListener("click", determineConverter);
+}
+
+const clearFields = (e) => {
+	document.getElementById('temperature').innerHTML = '';
+	document.getElementById("conversion-space").innterHTML = '';
+}
+
+
+button.addEventListener("click", determineConverter);
+resetButton.addEventListener("click", clearFields);
+
+
+// enter key has to activate functions
+// colors based on instructions
